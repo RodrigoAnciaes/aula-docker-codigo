55a2480c3cd4f05e807ed9cc03788a23d7e46080
RodrigoAnciaes <81170574+RodrigoAnciaes@users.noreply.github.com>
2024-09-07 16:43:04 -0300

    Code adjustments.

---
+++ src/create_db.py
@@ -0,0 +1,13 @@
+import sqlite3
+
+db = sqlite3.connect("quiz.db")
+print("Opened database successfully")
+
+cursor = db.cursor()
+
+startup_file = open("quiz.sql", "r")
+startup_script = startup_file.read()
+startup_file
+
+cursor.executescript(startup_script)
+
